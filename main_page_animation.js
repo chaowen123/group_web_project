435fb62f1a2ed63ce62276d724eb3186b98d9f4d
chaowen123 <104219083+chaowen123@users.noreply.github.com>
2025-05-07 16:34:21 -0700

    Add files via upload

---
+++ main_page_animation.js
@@ -0,0 +1,14 @@
+function startAnimation() {
+    document.body.classList.remove('animation-start');
+    void document.body.offsetWidth; 
+    document.body.classList.add('animation-start');
+}
+
+window.addEventListener('load', () => {
+    setTimeout(startAnimation, 1000);
+});
+
+
+window.addEventListener('resize', () => {
+    setTimeout(startAnimation, 500);
+});
