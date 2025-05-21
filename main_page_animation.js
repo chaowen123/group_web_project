8ea263b0e724d8ae8b11b11c8152e5cbf69b3be4
chaowen123 <chaowencao@gmail.com>
2025-05-21 10:52:09 -0700

    Initial commit

---
+++ main_page_animation.js
@@ -0,0 +1,16 @@
+function startAnimation() {
+    document.body.classList.remove('animation-start');
+    void document.body.offsetWidth; 
+    document.body.classList.add('animation-start');
+}
+
+window.addEventListener('load', () => {
+    setTimeout(startAnimation, 1000);
+});
+
+
+window.addEventListener('resize', () => {
+    setTimeout(startAnimation, 500);
+});
+
+
